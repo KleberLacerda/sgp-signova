453a665bd84257b52c59738d2b646a9a5edf8414
Kleber Lacerda <kl.lacerda@gmail.com>
2026-09-21 01:01:28 -0300

    Configuração Spring Boot

---
+++ src/main/java/br/com/signova/SgpApplication.java
@@ -0,0 +1,11 @@
+package br.com.signova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SgpApplication.class, args);
+    }
+}
